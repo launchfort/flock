f2aaad92cd80eaea7b7bf182ff32a92c6a826354
Darren Schnare <schnare.d@gmail.com>
2018-06-14 19:45:35 -0400

    Add test for upgrade CLI action
    Add test for upgrade CLI action.
    
    Fix test for rollback CLI action.
    
    Ref #17

---
+++ src/flock-cli/actions/upgrade/index.test.ts
@@ -0,0 +1,105 @@
+import * as Assert from 'assert'
+import * as Path from 'path'
+import * as FileSystem from 'fs'
+import { promisify } from 'util'
+import * as TestHelpers from 'test-helpers'
+import { upgrade } from './index'
+
+const writeFile = promisify(FileSystem.writeFile)
+const readFile = promisify(FileSystem.readFile)
+const stat = promisify(FileSystem.stat)
+const exists = fileName => stat(fileName).then(() => true).catch(error => {
+  if (error.code === 'ENOENT') {
+    return false
+  } else {
+    throw error
+  }
+})
+
+describe('flock-cli/actions/upgrade', function () {
+  let dir = null
+  beforeEach(async function () {
+    dir = await TestHelpers.tmpdir()
+  })
+
+  afterEach(function () {
+    return dir ? TestHelpers.rmdirp(dir) : null
+  })
+
+  it('should upgrade a project that used version 0.x', async function () {
+    const rcFileName = Path.join(dir, 'flockrc-.js')
+
+    await upgrade({ rcFileName })
+
+    const rcExists = await exists(rcFileName)
+    Assert.ok(rcExists, `${Path.basename(rcFileName)} does not exist`)
+
+    const rcText = await readFile(rcFileName)
+    Assert.ok(rcText.indexOf(`const migrationTableName = 'migrations'`) > 0)
+    Assert.ok(rcText.indexOf(`const migrationDir = 'migrations'`) > 0)
+  })
+
+  it('should upgrade a project that used version 1.x', async function () {
+    const rcFileName = Path.join(dir, 'flockrc-.js')
+    const yoRcFileName = Path.join(dir, '.yo-rc.json')
+    const yoRc = {
+      flock: {
+        promptValues: {
+          driver: 'flock:lib/drivers/pg',
+          migrationDir: 'miggrationDir',
+          migrationTable: 'migrationTable'
+        }
+      }
+    }
+
+    await writeFile(yoRcFileName, JSON.stringify(yoRc), { encoding: 'utf8' })
+    await upgrade({ yoRcFileName, rcFileName })
+
+    const yoRcExists = await exists(yoRcFileName)
+    Assert.notEqual(yoRcExists, '.yo-rc.json still exists')
+
+    const rcExists = await exists(rcFileName)
+    Assert.ok(rcExists, `${Path.basename(rcFileName)} does not exist`)
+
+    const rcText = await readFile(rcFileName)
+    Assert.ok(rcText.indexOf(`const migrationTableName = '${yoRc.flock.promptValues.migrationTable}'`) > 0)
+    Assert.ok(rcText.indexOf(`const migrationDir = '${yoRc.flock.promptValues.migrationDir}'`) > 0)
+  })
+
+  it('should upgrade a project that used version 2.x', async function () {
+    const rcFileName = Path.join(dir, 'flockrc-.js')
+    const cfgFileName = Path.join(dir, '.flockrc-.json')
+    const cfg = {
+      driver: 'flock:lib/drivers/pg',
+      migrationDir: 'miggrationDir!!',
+      migrationTable: 'migrationTable!!'
+    }
+
+    await writeFile(cfgFileName, JSON.stringify(cfg), { encoding: 'utf8' })
+    await upgrade({ cfgFileName, rcFileName })
+
+    const cfFileExists = await exists(cfgFileName)
+    Assert.notEqual(cfFileExists, `${Path.basename(cfgFileName)} still exists`)
+
+    const rcExists = await exists(rcFileName)
+    Assert.ok(rcExists, `${Path.basename(rcFileName)} does not exist`)
+
+    const rcText = await readFile(rcFileName)
+    Assert.ok(rcText.indexOf(`const migrationTableName = '${cfg.migrationTable}'`) > 0)
+    Assert.ok(rcText.indexOf(`const migrationDir = '${cfg.migrationDir}'`) > 0)
+  })
+
+  it('should leave a project that used version 3.x unmodified', async function () {
+    const rcFileName = Path.join(dir, 'flockrc-.js')
+    const rc = 'Hello World!'
+
+    await writeFile(rcFileName, rc, { encoding: 'utf8' })
+    await upgrade({ rcFileName })
+
+    const rcExists = await exists(rcFileName)
+    Assert.ok(rcExists, `${Path.basename(rcFileName)} does not exist`)
+
+    const rcText = await readFile(rcFileName)
+    Assert.strictEqual(rc, 'Hello World!')
+  })
+})
--- src/flock-cli/actions/upgrade/index.ts
+++ src/flock-cli/actions/upgrade/index.ts
@@ -1,7 +1,10 @@
 import * as FileSystem from 'fs'
 import * as Path from 'path'
+import { promisify } from 'util'
 
-export function upgrade ({ yoRcFileName = '.yo-rc.json', cfgFileName = '.flockrc.json', rcFileName = 'flockrc.js' } = {}) {
+const unlink = promisify(FileSystem.unlink)
+
+export async function upgrade ({ yoRcFileName = '.yo-rc.json', cfgFileName = '.flockrc.json', rcFileName = 'flockrc.js' } = {}) {
   let migrationDir = ''
   let migrationTable = ''
 
@@ -11,12 +14,14 @@ export function upgrade ({ yoRcFileName = '.yo-rc.json', cfgFileName = '.flockrc
       const cfg = require(Path.resolve(cfgFileName))
       migrationDir = cfg.migrationDir || 'migrations'
       migrationTable = cfg.migrationTable || 'migration'
+      await unlink(cfgFileName)
     // Upgrade from 1.x to 3.x
     } else if (FileSystem.existsSync(yoRcFileName)) {
       const yorc = require(Path.resolve(yoRcFileName))
       const { flock: { promptValues = {} } } = yorc
       migrationDir = promptValues.migrationDir || 'migrations'
       migrationTable = promptValues.migrationTable || 'migration'
+      await unlink(yoRcFileName)
     // Upgrade from 0.x to 3.x (assume project was using flock in the early form)
     } else {
       migrationDir = 'migrations'
